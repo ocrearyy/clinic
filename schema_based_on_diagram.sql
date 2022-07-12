83bcd55104a48997b0c23f44d417b927369b3bb2
arturo <ahpareja@gmail.com>
2022-07-12 11:52:37 -0500

    Add 3 more tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,24 @@ CREATE TABLE invoices
    CONSTRAINT "fk_invoices" FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE SET NULL,
    PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments
+(
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    type VARCHAR(100),
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items
+(
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT,
+    CONSTRAINT "fk_invoice" FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE SET NULL,
+    CONSTRAINT "fk_treatment" FOREIGN KEY (treatment_id) REFERENCES treatments(id) ON DELETE SET NULL,
+    PRIMARY KEY(id)
+);
